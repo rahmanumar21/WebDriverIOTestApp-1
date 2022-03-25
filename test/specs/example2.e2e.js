a365525e9c1cbe12b5891794cc9391aa33eab7bb
rahmangithub2 <rahman.github2@gmail.com>
2022-03-25 12:13:55 +0100

    Modify example2.e2e.js codes

--- test/specs/example2.e2e.js
+++ test/specs/example2.e2e.js
@@ -3,10 +3,10 @@ describe('Google Page', async () => {
           it('should open the URL and verify the title', async () => {
 
                     await browser.url("https://google.com/");
-                    let checkURI = await browser.getUrl();
+                    await browser.getUrl();
 
                     // expect to have url
-                    expect(checkURI).toHaveUrl("https://google.com/");
+                    expect(browser).toHaveUrl("https://google.com/");
 
           });
 
